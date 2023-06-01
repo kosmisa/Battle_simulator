9247140b2d9752eb2058b8a6fc933b36da85ea1c
kosmisa <vukasin.jevtic@gmail.com>
2023-06-01 22:14:55 +0200

    createArmy_fix

--- my-app/src/createArmy.js
+++ my-app/src/createArmy.js
@@ -1,11 +1,18 @@
+import weapons from "./weaponTypes";
 import Unit from "./unit";
-import unitProps from "./unitProps";
 
 const createArmy = () => {
   const armyUnits = [];
   while (armyUnits.length < 5) {
-    const units = new Unit(unitProps);
-    armyUnits.push(units);
+    const unitProps = {
+      health: Math.floor(Math.random() * 101) + 50,
+      attack: Math.floor(Math.random() * 11) + 40,
+      defense: Math.floor(Math.random() * 6) + 19,
+      speed: Math.floor(Math.random() * 10) + 1,
+      weapon: weapons[Math.floor(Math.random() * weapons.length)],
+    };
+    const unit = new Unit(unitProps);
+    armyUnits.push(unit);
   }
   return armyUnits;
 };
